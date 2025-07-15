73ee4ae1d518a5409223767bc79d49f63f557516
ALEKSEY <andrsmek@gmail.com>
2025-07-15 17:16:13 +0300

    lists enable

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useTelegram } from './hooks/useTelegram';
 import './App.css';
 import React, { useEffect } from 'react';
 import Header from './components/Header/Header'
+import {Form, Route, Routes} from 'react-router-dom'
+import ProductList from './components/ProductList/ProductList';
 
 const tg = window.Telegram.WebApp;
 function App() {
@@ -16,7 +18,11 @@ function App() {
 
     <div className="App">
       <Header />
-      <button onClick={onToggleButton}>toggle</button>
+      <Routes>
+        <Route index element = {<ProductList />}/>
+
+        <Route path={'form'} element = {<Form/>}/>
+      </Routes>
     </div>
   );
 }
